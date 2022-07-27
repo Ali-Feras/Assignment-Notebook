81d69a0cf223c7c7e8086d9c66d67718dd3cef4f
Ali Feras <ferasali876@gmail.com>
2022-07-27 12:51:42 -0500

    Redesigned list display

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems = [
+        AssignmentItem(course: "Math", description: "Unit 7 Test", dueDate: Date()),
+        AssignmentItem(course: "Physics", description: "Unit 8 Test", dueDate: Date()),
+        AssignmentItem(course: "English", description: "Unit 3 Test", dueDate: Date())
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(assignmentItems) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.course)
+                                .font(.headline)
+                            Text(item.description)
+                                .padding()
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                }
+                .onMove { indices, newOffset in
+                    assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                }
+                .onDelete { indexSet in
+                    assignmentItems.remove(atOffsets: indexSet)
+                }
+            }
+            .navigationBarTitle("Assignment List")
+            .navigationBarItems(leading: EditButton())
+
+        }
     }
 }
 
@@ -18,4 +46,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
